f1df10f606011a7d8c99caefcab40d823f603eb4
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-25 02:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const date = document.querySelector(".date")
+const hrs = document.querySelector(".hrs")
+const min = document.querySelector(".min")
+const sec = document.querySelector(".sec")
+const apm = document.querySelector(".apm")
+let d = new Date()
+let currentDate = d.getDate()
+let currentmonth = d.getMonth()
+let currentYear = d.getFullYear()
+let hr = d.getHours()
+let mi = d.getMinutes()
+let se = d.getSeconds()
+let total = `${currentmonth}/${currentDate}/${currentYear}`
+date.innerText = total
+let a = "AM"
+function change(){
+	if(se>=60){
+		se = 0
+		mi++
+	}
+	if(mi>=60){
+		mi = 0
+		hr++
+	}
+	if(hr>12){
+		hr = 1
+		a = "PM"
+	}
+	if(hr==12){
+		a = "PM"
+	}
+	hrs.innerText = `${hr}`.padStart(2,`0`)
+	min.innerText = `${mi}`.padStart(2,`0`)
+	sec.innerText = `${se++}`.padStart(2,`0`)
+	apm.innerText = a
+}
+setInterval(change,1000)
